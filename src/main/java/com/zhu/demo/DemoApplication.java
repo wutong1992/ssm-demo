fa09c796dcc105a50753d71194d463fe5ee959e3
zhutao <zhutao@mininglamp.com>
2020-05-23 15:07:35 +0800

    支持打包成war包方式

--- src/main/java/com/zhu/demo/DemoApplication.java
+++ src/main/java/com/zhu/demo/DemoApplication.java
@@ -3,16 +3,27 @@ package com.zhu.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * @author ZHUTAO
  */
 @SpringBootApplication
 @MapperScan("com.zhu.demo.dao")
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    /**
+     * 如此配置打包后可以war包才可在tomcat下使用
+     * @param builder
+     * @return
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
+    }
 }
